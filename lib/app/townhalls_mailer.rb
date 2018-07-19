34a7303e4cae9481a26c2e777b8d9af7565c1f33
maxime-manicourt <maxime.manicourt@gmail.com>
2018-07-19 19:48:45 +0200

    bon

--- lib/app/townhalls_mailer.rb
+++ lib/app/townhalls_mailer.rb
@@ -4,22 +4,22 @@ require 'gmail'
 class Mailing
 
 
-	USERNAME = 'husnot.a@gmail.com'
-	PASSWORD = 'Aeiouyteam61'
-	def global
+  USERNAME = 'husnot.a@gmail.com'
+  PASSWORD = 'Aeiouyteam61'
+
+  def global
 
 
+    gmail = Gmail.connect(USERNAME, PASSWORD)
 
-gmail = Gmail.connect(USERNAME, PASSWORD)
-
-edouard = ['edouard.plantevin@laposte.net', 'edouard.61@live.fr', 'bougaut.lea@gmail.com']
-edouard.each do |mail|
-	gmail.deliver do
-		to mail
-		subject "Ruby c'est cool"
-		html_part do
-			content_type 'text/html; charset=UTF-8'
-			body "<p>Bonjour,<br>
+    edouard = ['edouard.plantevin@laposte.net', 'edouard.61@live.fr', 'bougaut.lea@gmail.com']
+    edouard.each do |mail|
+      gmail.deliver do
+        to mail
+        subject "Ruby c'est cool"
+        html_part do
+          content_type 'text/html; charset=UTF-8'
+          body "<p>Bonjour,<br>
 Je m'appelle Edouard, je suis élève à The Hacking Project, une formation au code gratuite,
 sans locaux, sans sélection, sans restriction géographique.<br>
 La pédagogie de nôtre école est celle du peer-learning, où nous travaillons par petits groupes sur des projets concrets qui font apprendre le code.<br>
@@ -30,15 +30,15 @@ t du jour est d'envoyer (avec du codage) des emails aux mairies pour qu'ils nous
 
 
 <p>Charles, co-fondateur de The Hacking Project pourra répondre à toutes vos questions : 06.95.46.60.80</p>"
-		end
-		sleep(20)
-		puts "Message envoyer"
-	end
-end
+        end
+        sleep(20)
+        puts "Message envoyer"
+      end
+    end
 
-gmail.logout
+    gmail.logout
 
-end
+  end
 
 end
 
