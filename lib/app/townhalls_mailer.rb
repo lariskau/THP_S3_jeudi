c75627e4782671656f53d800bea8e3c212b4cbfb
Satan_petit_coeur <39798138+SatanPetitCoeur@users.noreply.github.com>
2018-07-19 19:57:37 +0200

    Ajout d'un décompte

--- lib/app/townhalls_mailer.rb
+++ lib/app/townhalls_mailer.rb
@@ -1,58 +1,19 @@
 require 'gmail'
 
-class Mailing
-
-
-  USERNAME = 'husnot.a@gmail.com'
-  PASSWORD = 'Aeiouyteam61'
-
-  def global
-
-
-    gmail = Gmail.connect(USERNAME, PASSWORD)
-
-    edouard = ['edouard.plantevin@laposte.net', 'edouard.61@live.fr', 'bougaut.lea@gmail.com']
-    edouard.each do |mail|
-      gmail.deliver do
-        to mail
-        subject "Ruby c'est cool"
-        html_part do
-          content_type 'text/html; charset=UTF-8'
-          body "<p>Bonjour,<br>
-Je m'appelle Edouard, je suis élève à The Hacking Project, une formation au code gratuite,
-sans locaux, sans sélection, sans restriction géographique.<br>
-La pédagogie de nôtre école est celle du peer-learning, où nous travaillons par petits groupes sur des projets concrets qui font apprendre le code.<br>
-Le proje
-t du jour est d'envoyer (avec du codage) des emails aux mairies pour qu'ils nous aident à faire de The Hacking Project un nouveau format d'éducation pour tous.</p>
-
-<p>Déjà 500 personnes sont passées par The Hacking Project. Est-ce que la mairie de [NOM_COMMUNE] veut changer le monde avec nous ?</p>
-
-
-<p>Charles, co-fondateur de The Hacking Project pourra répondre à toutes vos questions : 06.95.46.60.80</p>"
-        end
-        sleep(20)
-        puts "Message envoyer"
-      end
-    end
-
-    gmail.logout
-
-  end
-
-end
-
-Mailing.new.global
-
 #On se log à gmail =)
-  USERNAME = ''
-  PASSWORD = ''
+USERNAME = 'husnot.a@gmail.com'
+PASSWORD = 'Aeiouyteam61'
 
   def envoie_mail
 
     gmail = Gmail.connect(USERNAME, PASSWORD)
 #petit rapel @town_emails recense toutes les adresses e-mails des mairies d'un département
+#On créer une variable compte pour faire le décompte
+compte = @town_emails.length
 #On créer une boucle pour envoyer un mail à chaque mairies
+
     @town_emails.each do |mail|
+
       gmail.deliver do
       to mail
         subject "Apprendre à coder c'est cool"
@@ -63,15 +24,19 @@ Mailing.new.global
             sans locaux, sans sélection, sans restriction géographique.<br>
             La pédagogie de nôtre école est celle du peer-learning, où nous travaillons par petits groupes sur des projets concrets qui font apprendre le code.<br>
             Le projet du jour est d'envoyer (avec du codage) des emails aux mairies pour qu'ils nous aident à faire de The Hacking Project un nouveau format d'éducation pour tous.</p>
+
             <p>Déjà 500 personnes sont passées par The Hacking Project. Est-ce que votre mairie veut changer le monde avec nous ?</p>
+
             <p>Charles, co-fondateur de The Hacking Project pourra répondre à toutes vos questions : 06.95.46.60.80</p>"
         end
+        #Le fameux décompte
+        compte = compte - 1
         #On le fait dormir un peu, au cas ou ^^
          sleep(20)
-         puts "Message envoyer à #{mail}"
+         puts "Message envoyer à #{mail} il ne reste plus que #{compte} à envoyer"
       end
     end
 
   gmail.logout #On se déco de gmail
 
-end
+  end
