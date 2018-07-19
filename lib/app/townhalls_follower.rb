d9a2b3d4d1856303eb72c3339caf5b9d7e9eedb6
Manon Lan <manondoublier@yahoo.fr>
2018-07-19 23:44:58 +0200

    modif env pour scrapper et follower

--- lib/app/townhalls_follower.rb
+++ lib/app/townhalls_follower.rb
@@ -1,16 +1,18 @@
 require 'twitter'
 require 'dotenv'
 require_relative 'townhalls_scrapper.rb'
+require 'google_drive'
 
 def pro_twitter
 
-  #On se connecte avec le .env
+  #On se connecte avec nos clefs =)
   client = Twitter::REST::Client.new do |config|
     config.consumer_key        = ENV['CONFIG.CONSUMER_KEY']
     config.consumer_secret     = ENV['CONFIG.CONSUMER_SECRET']
     config.access_token        = ENV['CONFIG.ACCESS_TOKEN']
     config.access_token_secret = ENV['CONFIG.ACCESS_TOKEN_SECRET']
   end
+
 
 #On créer un tableau qui va contenir les derniers utilisateurs qui on parlais de leurs mairies
   user_follow = []
@@ -19,6 +21,16 @@ def pro_twitter
     client.search("mairie #{i}", result_type: "recent").take(1).collect do |tweet|
       nom = "@#{tweet.user.screen_name}" #on rajoute un "@"
       user_follow << nom
+#On enregistre les handles dans le googles spreadsheet accessible ici : https://docs.google.com/spreadsheets/d/1m5VF7W9d0NAtBSkAKh0mvJkcEaEJIA7vU-ATV7Q0iK8/edit?usp=sharing
+      session = GoogleDrive::Session.from_config("config.json")
+      ws = session.spreadsheet_by_key("1m5VF7W9d0NAtBSkAKh0mvJkcEaEJIA7vU-ATV7Q0iK8").worksheets[0]
+      i = 2
+
+      user_follow.each do |user|
+      ws[i, 4] = user
+      ws.save
+      i += 1
+      end
     end
   end
 
@@ -29,8 +41,11 @@ def pro_twitter
     begin
       client.follow!(single_user)
       puts "Vous venez de follow #{single_user}"
-    rescue Exception, NotFound, Forbidden #Evite que le programme s'arrete quand il ne trouve rien  
+    rescue Exception, NotFound, Forbidden #Evite que le programme s'arrete quand il ne trouve rien
     next
     end
   end
+  puts user_follow
 end
+
+pro_twitter
--- lib/app/townhalls_scrapper.rb
+++ lib/app/townhalls_scrapper.rb
@@ -1,37 +1,62 @@
+require 'rubygems'
 require 'nokogiri'
 require 'open-uri'
 require 'csv'
+require 'google_drive'
+require 'dotenv'
 
 require_relative 'townhalls_follower.rb'
 require_relative 'townhalls_mailer.rb'
   def get_the_email_of_a_townhal_from_its_webpage(url)
-      #Open the web page
+      #Ouverture de l'url du site de la ville
       page = Nokogiri::HTML(open(url))
-      #Scrap the e-mail address
+      #Scrapping des adresses e-mail
       page.xpath('//body/div/main/section[2]/div/table/tbody/tr[4]/td[2]').text
   end
 
+  def get_the_district(url)
+    #Ouverture de l'url du site de la ville
+    page = Nokogiri::HTML(open(url))
+    #Scrapping du département
+    page.xpath("//body/div/main/section[4]/div/table/tbody/tr[1]/td[2]").text
+  end
+
+
 #def pour scrapper toutes les mairies de la Manche
   def get_all_the_urls_of_manche
-#On créer deux tableau, 1 pour stocker les noms des maries, l'autre pour stocker les e-mails
+#On créé deux tableaux, 1 pour stocker les noms des maries, l'autre pour stocker les e-mails
+    array = []
+
     @town_names = []
     @town_emails = []
     urls = %w[http://www.annuaire-des-mairies.com/manche.html http://www.annuaire-des-mairies.com/manche-2.html http://www.annuaire-des-mairies.com/manche-3.html]
 
     urls.each do |url|
     doc = Nokogiri::HTML(open(url))
-  	doc.css(".lientxt").each do |link|
-  		url = "http://annuaire-des-mairies.com" + link["href"][1..-1].to_s
-  		town_name = link.text
-  		town_email = get_the_email_of_a_townhal_from_its_webpage(url)
+    doc.css(".lientxt").each do |link|
+      url = "http://annuaire-des-mairies.com" + link["href"][1..-1].to_s
+      town_name = link.text
+      town_email = get_the_email_of_a_townhal_from_its_webpage(url)
+      town_district = get_the_district(url)
+      ville = {:name => town_name, :email => town_email, :district => town_district}
+      array << ville
+
       @town_names << town_name
       @town_emails << town_email
       end
     end
 
-  #On répete les mêmes def pour le Var et Morbihan 
+    array
+    envoie_mail #Cette méthode se trouve dans townhalls_mailer.rb
+    pro_twitter #Cette méthode se trouve dans townhalls_follower.rb
+  end
+
+  #On répète les mêmes def pour le Var et Morbihan
+
 
   def get_all_the_urls_of_var
+    array = []
+
     @town_names = []
     @town_emails = []
     urls = %w[http://www.annuaire-des-mairies.com/var.html]
@@ -42,16 +67,27 @@ require_relative 'townhalls_mailer.rb'
       url = "http://annuaire-des-mairies.com" + link["href"][1..-1].to_s
       town_name = link.text
       town_email = get_the_email_of_a_townhal_from_its_webpage(url)
+      town_district = get_the_district(url)
+      ville = {:name => town_name, :email => town_email, :district => town_district}
+      array << ville
+
       @town_names << town_name
       @town_emails << town_email
+
+      print @town_names
       end
-    end
-    envoie_mail
-    pro_twitter
-  end
+      end
+      array
+      envoie_mail
+      pro_twitter
+      end
+
+
 
 
   def get_all_the_urls_of_morbihan
+    array = []
+
     @town_names = []
     @town_emails = []
     urls = %w[http://www.annuaire-des-mairies.com/morbihan.html]
@@ -62,10 +98,45 @@ require_relative 'townhalls_mailer.rb'
       url = "http://annuaire-des-mairies.com" + link["href"][1..-1].to_s
       town_name = link.text
       town_email = get_the_email_of_a_townhal_from_its_webpage(url)
+      town_district = get_the_district(url)
+      ville = {:name => town_name, :email => town_email, :district => town_district}
+      array << ville
+
       @town_names << town_name
       @town_emails << town_email
       end
     end
+    array
     envoie_mail
     pro_twitter
-end
+    end
+
+
+#On envoie tout dans un beau spreadsheet accessible ici : https://docs.google.com/spreadsheets/d/1m5VF7W9d0NAtBSkAKh0mvJkcEaEJIA7vU-ATV7Q0iK8/edit?usp=sharing
+
+  def spreadsheet(hash_array)
+
+    session = GoogleDrive::Session.new|config|
+      client_id = ENV['CLIENT_ID']
+      client_secret = ENV['CLIENT_SECRET']
+      scope = ENV['SCOPE']
+      refresh_token = ENV['REFRESH_TOKEN'] 
+
+
+    ws = session.spreadsheet_by_key("1m5VF7W9d0NAtBSkAKh0mvJkcEaEJIA7vU-ATV7Q0iK8").worksheets[0]
+    i = 2
+    # On donne des titres aux colonnes
+    ws[1, 1] = "Nom de ville"
+    ws[1, 2] = "Mail mairie"
+    ws[1, 3] = "Département"
+
+    # On écrit dans les colonnes les données
+    hash_array.each do |hash|
+
+      ws[i, 1] = hash[:name]
+      ws[i, 2] = hash[:email]
+      ws[i, 3] = hash[:district]
+      ws.save
+      i += 1
+     end
+  end
